d50c3547c6beb7fa23f20a9e7b56c90b0a4bed23
Anttoha <167896136+Antoha2012pro@users.noreply.github.com>
2025-11-12 17:04:44 +0100

    1

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import {minus, multiply} from "./js/minus"; // Другий варіант. Синхронизовується з назвою function. Може приймати багато функцій у одному файлі
+console.log(minus(10, 6));
---
+++ src/js/minus.js
@@ -0,0 +1,7 @@
+export function minus(a, b) { // Другий варіант
+    return a - b;
+}
+
+export function multiply(a, b) { // Другий варіант
+    return a * b;
+}
